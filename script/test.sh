92c3eab457840695ac9e610fea7730305acc299e
root <root@eddy1.localdomain>
2021-10-18 03:10:31 -0400

    script/test.sh

--- script/test.sh
+++ script/test.sh
@@ -1,1 +1,17 @@
-This is script test area
+# Meet Cluster Container
+echo -e "================================================================================================"
+echo -e "Docker Container List. Choose contanier name for redis cluster"
+docker ps -a
+echo -e "================================================================================================"
+echo Write the container name of 3 nodes to connect the cluster.
+read cname1 cname2 cname3
+
+echo -e "================================================================================================"
+echo -e "Choose contanier IP, port for redis cluster"
+ps -ef | grep redis
+echo -e "================================================================================================"
+echo Write the IP of the 3 nodes to connect the cluster.
+read ip1 ip2 ip3
+
+echo Write the port of the 3 nodes to connect the cluster.
+read port1 port2 port3
